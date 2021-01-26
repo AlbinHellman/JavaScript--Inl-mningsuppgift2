d0685192f1f7a47c07d3d00f1eb2e3f64b91cb2f
AlbinHellman <steelalbin@gmail.com>
2021-01-26 01:50:03 +0100

    JavaScript-Inlämningsuppgift2

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const Input = document.querySelector('#AddInput')
 const Knapp = document.querySelector('#Submit')
 const output = document.querySelector('#output')
 
-const Validatetext2 = id => {
+const validatetext2 = id => {
     const input = document.querySelector('#'+id);
     const error = document.querySelector('#'+id+'-error');
     if(input.id === "AddInput") {
         if(input.value === '') {
             texterror.innerText = 'Var vänligen att skriva in en text';
             return false;
-        } else if{input.value.length < 3) {
+        } else if(input.value.length < 3) {
             texterror.innerText = 'Texten måste innehålla minst tre tecken'
             return false;
         }
@@ -20,3 +20,20 @@ const Validatetext2 = id => {
         }
     }
 }
+
+const validate = () => {
+
+    document.querySelectorAll('input').forEach(input => {
+
+
+        if(input.id === "AddInput") {
+            validatetext2(input.id);
+        }
+    })
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    validate();
+} )
